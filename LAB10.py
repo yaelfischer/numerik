4a9a7b75f04219f5f3206838780c16cc7eb02d56
Yael Fischer <yael.fischer@hotmail.ch>
2023-04-20 11:25:18 +0200

    Lab10 Ok

--- LAB10.py
+++ LAB10.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy as sy
 
 
-def ExpRK(anfangs, X, N, f, but):
+def ImpRK(anfangs, X, N, f, but):
     counter = 0
     s = len(but[:,0])-1
     [x0, y0] = anfangs
@@ -17,8 +18,7 @@ def ExpRK(anfangs, X, N, f, but):
     for i in range(N):
         x.append(x[i] + h)
         for j in range(s):
-
-            k[j] = f(x[i]+but[j,0]*h,y[i]+h*np.dot(but[j,1:],k))
+            k[j] = sy.optimize.fsolve(lambda imp: f(x[i]+but[j,0]*h,y[i]+h*np.dot(but[j,1:j+1],k[0:j])+h*but[j,j+1]*imp)-imp, np.array([k[j-1]]))
             counter += 1
 
         y.append(y[i] + h*np.dot(k,but[-1,1:]))
@@ -28,31 +28,43 @@ def ExpRK(anfangs, X, N, f, but):
 
 
 #### Butcher-Tableau #####
-    # c1 | 0   0   0
-    # c2 | a21 0   0
-    # c3 | a31 a32 0
+    # c1 | a11 0   0
+    # c2 | a21 a22 0
+    # c3 | a31 a32 a33
     # ------------------
     #    | b1  b2  b3
+
+
+mittelPunktregel = [[0.5, 0.5],
+                    [0,   1]]
+mittelPunktregel = np.array(mittelPunktregel)
 
 # # butcher2x2
 # butcher = [[c1, a11, a12],
 #            [c2, a21, a22],
 #            [0,  b1,  b2]]
-runge = [[0,   0,   0],
-         [0.5, 0.5, 0],
-         [0,   0,   1]]
-runge = np.array(runge)
 
-heun = [[0, 0,   0],
-        [1, 1,   0],
-        [0, 0.5, 0.5]]
-heun = np.array(heun)
+trapezregel = [[0, 0,   0],
+               [1, 0.5, 0.5],
+               [0, 0.5, 0.5]]
+trapezregel = np.array(trapezregel)
+
+SDIRK2 = [[0.5+np.sqrt(3)/6, 0.5+np.sqrt(3)/6, 0],
+          [0.5-np.sqrt(3)/6, -np.sqrt(3)/3,    0.5+np.sqrt(3)/6],
+          [0,                0.5,              0.5]]
+SDIRK2 = np.array(SDIRK2)
 
 # # butcher3x3
 # butcher = [[c1, a11, a12, a13],
 #            [c2, a21, a22, a23],
 #            [c3, a31, a32, a33],
 #            [0,  b1,  b2,  b3]]
+xNor = 1.06858
+norsett = [[xNor, xNor, 0, 0],
+           [0.5, 0.5-xNor, xNor, 0],
+           [1-xNor, 2*xNor, 1-4*xNor, xNor],
+           [0,  1/(6*(1-2*xNor)**2),  ((3*(1-2*xNor)**2)-1)/(3*(1-2*xNor)**2), 1/(6*(1-2*xNor)**2)]]
+norsett = np.array(norsett)
 
 # # butcher4x4
 # butcher = [[c1, a11, a12, a13, a14],
@@ -60,12 +72,6 @@ heun = np.array(heun)
 #            [c3, a31, a32, a33, a34],
 #            [c4, a41, a42, a43, a44],
 #            [0,  b1,  b2,  b3,  b4]]
-RK4 = [[0.0, 0.0, 0.0, 0.0, 0.0],
-       [0.5, 0.5, 0.0, 0.0, 0.0],
-       [0.5, 0.0, 0.5, 0.0, 0.0],
-       [1.0, 0.0, 0.0, 1.0, 0.0],
-       [0,   1/6, 1/3, 1/3, 1/6]]
-RK4 = np.array(RK4)
 
 ## Test func
 # y' = -4y, y(0) = 1
@@ -73,22 +79,24 @@ def f1(x,y):
     return -4*y
 
 
-x1, y1, _ = ExpRK([0,1],1,10,f1,runge)
-x2, y2, _ = ExpRK([0,1],1,10,f1,heun)
-x3, y3, _ = ExpRK([0,1],1,10,f1,RK4)
+x1, y1, _ = ImpRK([0,1],1,10,f1,mittelPunktregel)
+x2, y2, _ = ImpRK([0,1],1,10,f1,trapezregel)
+x3, y3, _ = ImpRK([0,1],1,10,f1,SDIRK2)
+x4, y4, _ = ImpRK([0,1],1,10,f1,norsett)
 
 analytic = np.exp(-4 * x1)
 
 plt.plot(x1,y1)
-plt.plot(x2,y2, linestyle='dashed')
+plt.plot(x2,y2)
 plt.plot(x3,y3)
+plt.plot(x4,y4)
 plt.plot(x1,analytic,linestyle='dashed')
 plt.xlabel('x')
 plt.ylabel('y')
-plt.legend(['Runge', 'Heun', 'RK4', 'Analytic'])
+plt.legend(['Mittelpunkt', 'Trapez', 'SDIRK2', 'Norsett', 'Analytic'])
 plt.show()
 
-## Func
+
 def f2(x,y):
     return -(x**2)/y
 
@@ -100,35 +108,32 @@ endVal = -4*np.sqrt(2/3)
 x1Err = []
 x2Err = []
 x3Err = []
+x4Err = []
 
 count1 = []
 count2 = []
 count3 = []
+count4 = []
 
 for i in Ns:
-    x1, y1, c1 = ExpRK([0, -4], 2, i, f2, runge)
-    x2, y2, c2 = ExpRK([0, -4], 2, i, f2, heun)
-    x3, y3, c3 = ExpRK([0, -4], 2, i, f2, RK4)
+    x1, y1, c1 = ImpRK([0, -4], 2, i, f2, mittelPunktregel)
+    x2, y2, c2 = ImpRK([0, -4], 2, i, f2, trapezregel)
+    x3, y3, c3 = ImpRK([0, -4], 2, i, f2, SDIRK2)
+    x4, y4, c4 = ImpRK([0, -4], 2, i, f2, norsett)
     count1.append(c1)
     count2.append(c2)
     count3.append(c3)
+    count4.append(c4)
     x1Err.append(np.abs(endVal - y1[-1]))
     x2Err.append(np.abs(endVal - y2[-1]))
     x3Err.append(np.abs(endVal - y3[-1]))
+    x4Err.append(np.abs(endVal - y4[-1]))
 
-plt.subplot(2,1,1)
 plt.loglog(Ns, x1Err, 'o-')
 plt.loglog(Ns, x2Err, 'o-')
 plt.loglog(Ns, x3Err, 'o-')
+plt.loglog(Ns, x4Err, 'o-')
 plt.xlabel('Anzahl Schritte')
 plt.ylabel('Fehler')
-plt.legend(['Runge', 'Heun', 'RK4'])
-
-plt.subplot(2,1,2)
-plt.loglog(count1, x1Err, 'o-')
-plt.loglog(count2, x2Err, 'o-')
-plt.loglog(count3, x3Err, 'o-')
-plt.xlabel('Aufwand')
-plt.ylabel('Fehler')
-plt.legend(['Runge', 'Heun', 'RK4'])
+plt.legend(['Mittelpunkt', 'Trapez', 'SDIRK2', 'Norsett'])
 plt.show()
